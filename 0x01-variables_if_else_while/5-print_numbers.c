35c53860dafdfb635c2822277bbd403eb021fc32
Chideraa <chideraaozobu@gmail.com>
2023-02-21 12:41:23 -0800

    conditions

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	num = 0;
+	while (num < 11)
+	{
+		printf("%d", num);
+	}
+	printf("\n");
+	return (0);
+}
