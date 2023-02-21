0263e7eb86ffbce122175cb03d5cbb92c93f97a7
Chideraa <chideraaozobu@gmail.com>
2023-02-21 09:05:43 -0800

    conditions

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch;
+
+	ch = 'a';
+
+	while (ch < 123)
+	{
+		putchar(ch);
+		ch++;
+	}
+	return (0);
+}
