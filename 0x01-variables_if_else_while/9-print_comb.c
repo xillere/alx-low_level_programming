853fb13d13e7b5ecc8f68c5942ad839fb1b5fe8f
Chideraa <chideraaozobu@gmail.com>
2023-02-21 13:14:05 -0800

    conditions

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	num = '0';
+
+	while (num < 58)
+	{
+		putchar(num);
+		putcar(',');
+		putchar(' ');
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
