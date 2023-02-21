1a3a550c058641fabdb7c9eef9cd53d795ce7612
Chideraa <chideraaozobu@gmail.com>
2023-02-21 07:44:40 -0800

    conditions

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -28,7 +28,7 @@ int main(void)
 	}
 	else
 	{
-		printf("Last digit od %d is %d and is less than 6 and not 0\n", n, lastDigit);
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
 	}
 	return (0);
 }
