7ca7bfac8b0ffb29664d98ad87284489b756d785
Chideraa <chideraaozobu@gmail.com>
2023-02-21 15:31:43 -0800

    nested

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char message[] = "_putchar";
+	int i = 0;
+
+	while (i < 8)
+	{
+		putchar(message[i]);
+	}
+	putchar('\n');
+	return (0);
+}
