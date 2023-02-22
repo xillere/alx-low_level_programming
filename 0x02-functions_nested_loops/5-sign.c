aab5cea1c015e632eafcc6278703a8ad7b8bc16c
Chideraa <chideraaozobu@gmail.com>
2023-02-22 12:45:58 -0800

    nested

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * print_sign - Entry point
+ * @n: input
+ * Return: Always 0 (Success)
+ */
+int print_sign(int n)
+{
+	int num = n;
+
+	if (num > 0)
+	{
+		putchar('+');
+		return (1);
+	}
+	else if (num == 0)
+	{
+		putchar('0');
+		return (0);
+	}
+	else
+	{
+		return (0);
+	}
+}
