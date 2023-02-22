857e3674a710e16fed0d273dc74dc5955e152c05
Chideraa <chideraaozobu@gmail.com>
2023-02-21 17:10:19 -0800

    nested

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,9 +7,11 @@
 void print_alphabet_x10(void)
 {
     int ch = 'a';
+    int i = 0;
 
-    for (int i = 0; i < 10; i++)
+    while (i < 11)
     {
+	    i++;
 	    while (ch < 123)
 	    {
 		    putchar(ch);
