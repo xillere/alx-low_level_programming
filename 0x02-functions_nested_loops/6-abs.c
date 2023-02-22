4f74f785307ecc7421d8d9969955bc332460a8f9
Chideraa <chideraaozobu@gmail.com>
2023-02-22 14:02:13 -0800

    nested

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _abs - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int _abs(int)
+{
+	num = int;
+
+	abnum = abs(num);
+
+	return (abnum);
+}
