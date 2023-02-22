323d5b389acf8981f6bdd8bc0d970b2c9c9901bf
Chideraa <chideraaozobu@gmail.com>
2023-02-21 18:15:52 -0800

    nested

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int _isalpha(int c)
+{
+	ch = c;
+
+	if (ch > 64 && ch < 91 && ch > 96 && ch <123)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
