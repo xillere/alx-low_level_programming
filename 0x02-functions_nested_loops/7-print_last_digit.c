9576a08ce3a10fa6973bddfd0a60729aa18a1265
Chideraa <chideraaozobu@gmail.com>
2023-02-22 14:11:59 -0800

    nested

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * print_last_digit - Entry point
+ * @n: input
+ * Return: Always 0 (Success)
+ */
+int print_last_digit(int n)
+{
+	int lnum = n % 10;
+	return (lnum);
+}
