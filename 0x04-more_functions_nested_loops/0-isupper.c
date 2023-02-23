de97f695a56e29eb4328b769c383b88784a2eb0c
Chideraa <chideraaozobu@gmail.com>
2023-02-23 07:52:39 -0800

    more nested

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * _isupper - Entry point
+ * @c: input
+ * Return: Always 0 (Success)
+ */
+int _isupper(int c)
+{
+	if (c > 64 && c < 91)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
