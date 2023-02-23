30fb735588ea78805740b1eabb96ceab83930354
Chideraa <chideraaozobu@gmail.com>
2023-02-23 08:29:11 -0800

    more nested

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 /**
  * mul - Entry point
- * a, b: input
+ * @a: input
+ * @b: input
  * Return: Always 0 (Success)
  */
 int mul(int a, int b)
 {
 	int product = a * b;
+
 	return (product);
 }
