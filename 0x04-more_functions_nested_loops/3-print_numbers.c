be4ee5b90abeb03bc98bc17fa37d7f6a3a28b314
Chideraa <chideraaozobu@gmail.com>
2023-02-23 10:07:22 -0800

    more nested

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * print_numbers - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_numbers(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
