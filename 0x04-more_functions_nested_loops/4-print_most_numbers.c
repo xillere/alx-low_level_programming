68fd445796e90095047f616cbba640237d758a6e
Chideraa <chideraaozobu@gmail.com>
2023-02-23 10:37:05 -0800

    more nested

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * print_most_numbers - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_most_numbers(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		if (i != 50 && i != 52)
+		{
+			putchar(i);
+			i++;
+		}
+	}
+	putchar('\n');
+}
