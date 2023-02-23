e15ea17e542556a1046fa49a6a8e5c69b2745cd4
Chideraa <chideraaozobu@gmail.com>
2023-02-23 08:16:08 -0800

    more nested

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @c: input
+ * Return: Always 0 (Success)
+ */
+int _isdigit(int c)
+{
+	int num = c;
+
+	if (num >= 0 && num <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
