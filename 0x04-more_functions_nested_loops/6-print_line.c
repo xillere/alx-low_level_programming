0fe6c1839c5642c41a3d09c2e117dc8f2900ddcd
Chideraa <chideraaozobu@gmail.com>
2023-02-23 15:39:44 -0800

    more nested

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * print_line - Entry point
+ * @n: input
+ * Return: Always 0 (Success)
+ */
+void print_line(int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		putchar('_');
+	}
+}
