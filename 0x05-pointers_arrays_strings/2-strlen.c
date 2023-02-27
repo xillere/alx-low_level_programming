2afd08a57725165a4d51dd08ca31e9a81988478e
Chideraa <chideraaozobu@gmail.com>
2023-02-27 11:05:41 -0800

    pas

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * _strlen - Entry point
+ * @s: character
+ * Return: Always 0 (Success)
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != 0; i++)
+	{
+	}
+	return (i);
+}
