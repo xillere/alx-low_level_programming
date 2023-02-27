1e11db558b46706ffa262d52cdba4be0ccd83c21
Chideraa <chideraaozobu@gmail.com>
2023-02-27 12:24:42 -0800

    pas

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,12 +8,12 @@ void print_rev(char *s)
 {
 	int i;
 
-	for (i = 0; s[i] != 0; i++)
+	for (i = 0; s[i] != '\n'; i++)
 	{
 	}
 	for (; i >= 0; i--)
 	{
 		putchar(s[i]);
 	}
-	/*putchar('\n');*/
+	putchar('\n');
 }
