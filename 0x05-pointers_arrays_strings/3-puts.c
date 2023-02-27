0414143f259d0e78a302b77e6917c97e9d75cce7
Chideraa <chideraaozobu@gmail.com>
2023-02-27 11:38:45 -0800

    pas

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * _puts - Entry point
+ * @str: string
+ * Return: Always 0 (Success)
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; ; i++)
+	{
+		putchar(str[i]);
+	}
+	putchar('\n');
+}
