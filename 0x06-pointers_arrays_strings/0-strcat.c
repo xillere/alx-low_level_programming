94a36462fa462b21fed9c59cd0caf7a191337bda
Chideraa <chideraaozobu@gmail.com>
2023-03-01 10:00:03 -0800

    mpas

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * *_strcat - Entry point
+ * @dest: final string
+ * @src: string to be added
+ * Return: Always 0 (Success)
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int z;
+	int x;
+
+	z = strlen(dest);
+	x = strlen(src);
+
+	for (i = 0; i <= x; i++)
+	{
+		dest[z + i] = src[i]
+	}
+	return (dest);
+}
