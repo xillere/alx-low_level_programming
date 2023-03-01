33dd16096c05a3f9fd4ca810aa12797cedd11f1e
Chideraa <chideraaozobu@gmail.com>
2023-03-01 10:31:06 -0800

    mpas

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * *_strncat - Entry point
+ * @dest: final string
+ * @src: string to be added
+ * @n: integer
+ * Return: Always 0 (Success)
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int z;
+
+	z = strlen(dest);
+
+	for (i = 0; i <= n; i++)
+	{
+		dest[z + i] = src[i];
+	}
+	return (dest);
+}
