daf18d15b24d0878c0f5a3e1e8050e9e2e71ce3b
Chideraa <chideraaozobu@gmail.com>
2023-03-02 03:04:53 -0800

    mpas

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
 /**
  * *_strncpy - Entry point
- *
+ * @dest: final string
+ * @src: original string
+ * @n: bytes to be copied
  * Return: Always 0 (Success)
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	size_t i;
+	int i;
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
 		dest[i] = src[i];
+	}
 	for ( ; i < n; i++)
+	{
 		dest[i] = '\0';
+	}
 
 	return (dest);
 }
