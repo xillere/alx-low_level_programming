d543b5825a63efb8b678be7ce523c653e0b6d94f
Chideraa <chideraaozobu@gmail.com>
2023-03-02 13:38:54 -0800

    mpas

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -26,7 +26,8 @@ char *cap_string(char *str)
 					str[i - 1] == '(' ||
 					str[i - 1] == ')' ||
 					str[i - 1] == '{' ||
-					str[i - 1] == '}')
+					str[i - 1] == '}' ||
+					i == 0)
 			str[i] = str[i] - 32;
 		}
 	}
