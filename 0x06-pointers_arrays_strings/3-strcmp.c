f9386c44c5d661f74a403589ff57598265e01979
Chideraa <chideraaozobu@gmail.com>
2023-03-02 07:21:05 -0800

    mpas

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strcmp - Entry point
+ * @s1: string
+ * @s2: anada string
+ * Return: Always 0 (Success)
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int a, b;
+
+	a = strlen(s1);
+	b = strlen(s2);
+
+	if (a == b)
+	{
+		return (0);
+	}
+	if (a > b)
+	{
+		return (a - b);
+	}
+	if (b > a)
+	{
+		return (a -b)
+	}
+}
