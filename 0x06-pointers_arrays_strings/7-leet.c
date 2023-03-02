81bc44f35e8c293901620d22cb4341fee874cccc
Chideraa <chideraaozobu@gmail.com>
2023-03-02 14:23:14 -0800

    mpas

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -12,31 +12,28 @@ int i;
 
 for (i = 0; i < len; i++)
 {
-	if (str[i] >= 'a' && str[i] <= 'z')
+	if (str[i] == 'a' || str[i] == 'A')
 	{
-		if (str[i] == 'a' || str[i] == 'A')
-		{
-			str[i] = '4';
-		}
-		else if (str[i] == 'e' || str[i] == 'E')
-		{
-			str[i] = '3';
-		}
-		else if (str[i] == 'o' || str[i] == 'O')
-		{
-			str[i] = '0';
-		}
-		else if (str[i] == 't' || str[i] == 'T')
-		{
-			str[i] = '7';
-		}
-		else if (str[i] == 'l' || str[i] == 'L')
-		{
-			str[i] = '1';
-		}
-		else
-		{
-		}
+		str[i] = '4';
+	}
+	else if (str[i] == 'e' || str[i] == 'E')
+	{
+		str[i] = '3';
+	}
+	else if (str[i] == 'o' || str[i] == 'O')
+	{
+		str[i] = '0';
+	}
+	else if (str[i] == 't' || str[i] == 'T')
+	{
+		str[i] = '7';
+	}
+	else if (str[i] == 'l' || str[i] == 'L')
+	{
+		str[i] = '1';
+	}
+	else
+	{
 	}
 }
 return (str);
