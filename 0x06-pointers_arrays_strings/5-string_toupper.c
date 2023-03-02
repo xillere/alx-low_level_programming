a08c024390d554306f8f91aad1c157795d322bc7
Chideraa <chideraaozobu@gmail.com>
2023-03-02 13:05:20 -0800

    mpas

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+/**
+ * string_toupper - Entry point
+ * @*: pointer
+ * Return: Always 0 (Success)
+ */
+char *string_toupper(char *str)
+{
+	int i;
+	int c;
+
+	c = strlen(str);
+
+	for (i = 0; i < c; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+	}
+	return (str);
+}
