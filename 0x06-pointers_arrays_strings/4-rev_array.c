406f0331f3ac46f4c28cb3942d64b68723162f02
Chideraa <chideraaozobu@gmail.com>
2023-03-02 10:20:57 -0800

    mpas

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * reverse_array - Entry point
+ * @a: array
+ * @n: number of elements
+ * Return: Always 0 (Success)
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+	int owari;
+	int copy[n];
+
+	owari = n;
+
+	for (i = 0; i <= n; i++)
+	{
+		copy[i] = a[i];
+	}
+	for (i = 0; i <= n; i++, owari--)
+	{
+		a[i] = copy[owari];
+	}
+}
