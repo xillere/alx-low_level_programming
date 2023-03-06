6556c43cc6513a5c0c00396816d95c568ef67ebd
Chideraa <chideraaozobu@gmail.com>
2023-03-06 07:44:34 -0800

    2d

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,18 +7,30 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	int i, x, c;
+	int i, x, c, boo;
 
 	c = 0;
 	for (i = 0; s[i] != '\0'; i++)
 	{
+		boo = 0;
 		for (x = 0; accept[x] != '\0'; x++)
 		{
 			if (s[i] == accept[x])
 			{
 				c++;
+				boo = 1;
+				break;
 			}
 		}
+		if (boo != 1)
+		{
+			break;
+		}
+		else
+		{
+			c++;
+		}
+
 	}
 	return (c);
 }
