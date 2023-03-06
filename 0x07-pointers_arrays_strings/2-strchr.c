24799eae7deefef607307c5a4f0d4934371237a1
Chideraa <chideraaozobu@gmail.com>
2023-03-06 06:26:41 -0800

    2d

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * _strchr - Entry point
+ * @s: string
+ * @c: character to search
+ * Return: Always 0 (Success)
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+	return (NULL);
+}
