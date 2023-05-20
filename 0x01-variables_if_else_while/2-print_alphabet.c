9ef91367b345c0c19b44478b07baa86d1448722d
Chideraa Ozobu <chideraaozobu@gmail.com>
2023-05-19 17:51:55 -0700

    first c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	i=61;
+	while (i < 123)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
