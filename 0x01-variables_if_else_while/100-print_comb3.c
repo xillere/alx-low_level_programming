0baa72c2e88b5306ec78b3540466dec39dad3ae5
Chideraa Ozobu <chideraaozobu@gmail.com>
2023-05-19 18:51:46 -0700

    first c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int f, s;
+
+	for (f = '0'; f < '9'; f++)
+	{
+		for (s = f + 1; s <= '9'; s++)
+		{
+			if (f != s)
+			{
+				putchar(f);
+				putchar(s);
+				if (f == 8 && s == 9)
+				{
+					continue;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
