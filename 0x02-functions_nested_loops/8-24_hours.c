b99da6485df56f0f34ab7528b5b5d322546423ca
Chideraa Ozobu <chideraaozobu@gmail.com>
2023-05-23 18:34:07 -0700

    nested

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @h, m: var
+ * Return: Always 0 (Success)
+ */
+void jack_bauer(void)
+{
+	int h, m;
+
+	for (h = 0; h <= 23; h++)
+	{
+		for (m = 0; m <= 59; m++)
+		{
+			putchar((h / 10) + '0');
+			putchar((h % 10) + '0');
+			putchar(':');
+			putchar((m / 10) + '0');
+			putchar((m % 10) + '0');
+			putchar('\n');
+		}
+	}
+}
