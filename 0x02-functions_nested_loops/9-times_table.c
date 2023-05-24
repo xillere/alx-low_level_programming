9cac5d2e33a7508d1c1ed0558133a20c66fa6d7e
Chideraa Ozobu <chideraaozobu@gmail.com>
2023-05-23 18:39:50 -0700

    nested

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,22 +10,22 @@ void times_table(void)
 
 	for (num = 0; num <= 9; num++)
 	{
-		_putchar('0');
+		putchar('0');
 
 		for (mult = 1; mult <= 9; mult++)
 		{
-			_putchar(',');
-			_putchar(' ');
+			putchar(',');
+			putchar(' ');
 
 			prod = num * mult;
 
 			if (prod <= 9)
-				_putchar(' ');
+				putchar(' ');
 			else
-				_putchar((prod / 10) + '0');
+				putchar((prod / 10) + '0');
 
-			_putchar((prod % 10) + '0');
+			putchar((prod % 10) + '0');
 		}
-		_putchar('\n');
+		putchar('\n');
 	}
 }
