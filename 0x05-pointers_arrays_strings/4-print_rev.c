a129eaf9bc1e6e1de7c3598f3cf172aabfa070cf
Chideraa Ozobu <chideraaozobu@gmail.com>
2023-05-30 18:22:49 -0700

    pointer

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_rev - reverse string
+ * @s: source
+ */
+void print_rev(char *s)
+{
+	int i, j, len;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	len = i;
+
+	for (j = len - 1; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+
+	_putchar('\n');
+}
