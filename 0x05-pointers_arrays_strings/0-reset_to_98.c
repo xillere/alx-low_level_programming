7cadff339deef4af982cb0f97d3270d89fdd9f4b
Chideraa Ozobu <chideraaozobu@gmail.com>
2023-05-30 17:49:03 -0700

    pointer

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,21 +2,11 @@
 #include "main.h"
 
 /**
- * strcat - function
- * @dest: pointer
- * @src: source
- *
- * Return: A pointer to the destination string @dest.
+ * reset_to_98 - updates the value of the variable
+ * the pointer points to to 98
+ * @n: pointer to the variable to update
  */
-char *strcat(char *dest, const char *src)
+void reset_to_98(int *n)
 {
-	int index = 0, dest_len = 0;
-
-	while (dest[index++])
-		dest_len++;
-
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
-
-	return (dest);
+	*n = 98;
 }
