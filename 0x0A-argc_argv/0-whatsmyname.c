dc51fa6e33ef32872e66ee12eb4ff1e40adab358
Chideraa Ozobu <chideraaozobu@gmail.com>
2023-06-15 01:35:04 -0700

    arg

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,8 +4,9 @@
  *
  * Return: Always 0 (Success)
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf(argv[0]);
+	printf("%s/n",argv[0]);
+
 	return (0);
 }
