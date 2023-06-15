12fdb3314b8db535353796c3906689622f1d4349
Chideraa Ozobu <chideraaozobu@gmail.com>
2023-06-15 01:46:55 -0700

    arg

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: the count of strings
+ * @argv: the array of strings
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printff("%d\n", argc);
+	return (0);
+}
