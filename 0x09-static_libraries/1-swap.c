e0550cc96d129deaf26a3c27ed73a7c347ebeb68
Chideraa Ozobu <chideraaozobu@gmail.com>
2023-06-20 04:56:30 -0700

    lib

---
+++ 0x09-static_libraries/1-swap.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * swap_int - swaps
+ * @a: pointer to first value
+ * @b: pointer to second value
+ */
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
