dca9065cf83048db7b2579bb2e920634877eb9b0
Chideraa Ozobu <chideraaozobu@gmail.com>
2023-06-30 12:59:04 -0700

    llt1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,10 +2,9 @@
 #include "lists.h"
 
 /**
- * list_len - returns the number of elements in a linked list
- * @h: pointer to the list_t list
- *
- * Return: number of elements in h
+ * list_len - returns no.
+ * @h: pointer
+ *Return: number of elements in h
  */
 size_t list_len(const list_t *h)
 {
