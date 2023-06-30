d4a0bb6e1280da342c8194412508cdab0b1adf80
Chideraa Ozobu <chideraaozobu@gmail.com>
2023-06-30 12:26:51 -0700

    ll

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_list - prints list
+ * @h: pointer 
+ * Return: no of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t i;
+
+	i = 0;
+
+	while (h)
+	{
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		i++;
+	}
+
+	return (i);
+}
