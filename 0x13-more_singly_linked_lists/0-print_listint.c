79bb9e9b2a7cbf2c556014b7af3db4f00fdcd213
Chideraa Ozobu <chideraaozobu@gmail.com>
2023-07-05 14:14:15 -0700

    t0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * print_listint - prints
+ * @h: link
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t n = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		n++;
+		h = h->next;
+	}
+
+	return (n);
+}
