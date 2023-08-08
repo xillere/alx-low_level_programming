6a32a15159e13fd524d292211dfa07be66c931c9
Chideraa <chideraaozobu@gmail.com>
2023-08-07 18:06:25 -0700

    working out file io

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile - Reads
+ * @filename: pointer filename
+ * @letters: letters to read and print.
+ * Return: on failure or NULL - 0
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t op, rd, wr;
+	char *buff;
+
+	if (filename == NULL)
+		return (0);
+
+	buff = malloc(sizeof(char) * letters);
+	if (buff == NULL)
+		return (0);
+
+	op = open(filename, O_RDONLY);
+	rd = read(op, buff, letters);
+	wr = write(STDOUT_FILENO, buff, rd);
+
+	if (op == -1 || rd == -1 || wr == -1 || wr != rd)
+	{
+		free(buff);
+		return (0);
+	}
+
+	free(buff);
+	close(op);
+
+	return (wr);
+}
