63076df0b9a1d9a94b9406f8761934df6aaebff8
Chideraa <chideraaozobu@gmail.com>
2023-11-03 08:52:10 -0700

    create

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,6 +1,8 @@
 #include <hash_tables.h>
+
 /**
- * key_index - return the index at which the key/value is stored in the ht array
+ * key_index - return the index at which the key/value
+ * is stored in the ht array
  * @key: key to be inserted in the hash function
  * @size: size of the array hash table
  *
@@ -8,5 +10,5 @@
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	return (hash_djb2(key) % size);
+	return ((hash_djb2(key) % size));
 }
