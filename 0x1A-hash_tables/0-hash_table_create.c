ac93a325041c9d8cdcdb07835263554ce4511cc4
Chideraa <chideraaozobu@gmail.com>
2023-11-03 04:13:53 -0700

    create ht

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,18 +1,22 @@
 #include "hash_tables.h"
+
 /*
  * hash_table_create - creates a new hash table
  * @size: size of ther hashtable
  *
  * Return: pointer to new hash table
  **/
+
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	int i;
-	hash_table_t *ht = (hash_table_t *) malloc(sizeof(hash_table_t));
+	hash_table_t *ht;
+
+	ht = (hash_table_t *) malloc(sizeof(hash_table_t));
 
 	if (ht == NULL)
 	{
-		return NULL;
+		return (NULL);
 	}
 	ht->size = size;
 	ht->array = (hash_node_t **) calloc(ht->size, sizeof(hash_node_t *));
