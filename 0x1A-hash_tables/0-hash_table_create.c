f1c247482415b50097a9665a9bce22f3b759f059
Chideraa <chideraaozobu@gmail.com>
2023-11-03 04:00:54 -0700

    hash table create

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,8 +8,8 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	int i;
-
 	hash_table_t *ht = (hash_table_t *) malloc(sizeof(hash_table_t));
+
 	if (ht == NULL)
 	{
 		return NULL;
@@ -19,5 +19,5 @@ hash_table_t *hash_table_create(unsigned long int size)
 	for (i = 0; i < ht->size; i++)
 		ht->items[i] = NULL;
 
-	return ht;
+	return (ht);
 }
